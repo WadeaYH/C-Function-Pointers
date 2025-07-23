0926de8bfa2fdc67ea39f4ef821ee3a91ec84c1c
WADI' YAWANSEH <wadi.yawanseh@c1r2s1.irbid.42.school>
2025-07-23 13:33:40 +0300

    STUPID ERROR

--- ex0/ft_foreach.c
+++ ex0/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: wadi.yawanseh <wadi.yawanseh@learner.42.t  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 10:49:37 by wadi.yawanseh     #+#    #+#             */
-/*   Updated: 2025/07/23 11:51:55 by wadi.yawanseh    ###   ########.fr       */
+/*   Updated: 2025/07/23 13:32:35 by wadi.yawanseh    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -16,6 +16,6 @@ void	ft_foreach(int *tab, int length, void (*f)(int))
 	int	i;
 
 	i = -1;
-	while (i++ < length)
+	while (i++ < (length - 1))
 		f(tab[i]);
 }
